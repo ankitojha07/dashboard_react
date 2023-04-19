1cd3ddb5cc3063c07992af3fcbf514ef7f8f8fb5
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-19 22:31:41 +0530

    card component mapped using map function

---
+++ src/data/card.js
@@ -0,0 +1,25 @@
+import { BiLike } from "react-icons/bi";
+import { BsCashCoin } from "react-icons/bs";
+
+export const cardData = [
+    {
+        title:'Total Revenue',
+        numeric : '$2,129,430',
+        icon : <BsCashCoin />
+    },
+    {
+        title:'Total Transactions   ',
+        numeric : '1,520',
+        icon : <BsCashCoin />
+    },
+    {
+        title:'Total Likes',
+        numeric : '9,721',
+        icon : <BiLike />
+    },
+    {
+        title:'Total Users',
+        numeric : '892',
+        icon : <BsCashCoin />
+    }
+]
