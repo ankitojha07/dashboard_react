53c9df21d858587c499cb64d7f922ea49ecfcc01
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-20 10:53:04 +0530

    removed warnings from app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
-import PiChart from "./components/graphs/PiChart";
 
 function App() {
   return (
